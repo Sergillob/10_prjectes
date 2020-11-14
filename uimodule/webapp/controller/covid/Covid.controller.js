641619f28e9c063beed50008d413e27bb88e2822
Llobet Mas <sergi.llobet@atos.net>
2020-11-14 13:49:23 +0100

    Primer commit

---
+++ uimodule/webapp/controller/covid/Covid.controller.js
@@ -0,0 +1,16 @@
+sap.ui.define([
+  "com/te/teProjects/controller/BaseController",
+  "sap/ui/model/json/JSONModel"
+], function (Controller, JSONModel) {
+  "use strict";
+
+  return Controller.extend("com.te.teProjects.controller.covid.Covid", {
+    onInit: function () {
+
+
+
+    }
+
+
+  });
+});
