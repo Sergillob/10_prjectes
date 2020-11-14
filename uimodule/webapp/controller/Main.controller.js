42a3c216f2617a1d066e38d5b5a27d3d33165ac6
Llobet Mas <sergi.llobet@atos.net>
2020-11-14 13:48:11 +0100

    Primer commit

--- uimodule/webapp/controller/Main.controller.js
+++ uimodule/webapp/controller/Main.controller.js
@@ -1,6 +1,6 @@
 sap.ui.define([
   "com/te/teProjects/controller/BaseController"
-], function(Controller) {
+], function (Controller) {
   "use strict";
 
   return Controller.extend("com.te.teProjects.controller.Main", {});
